3b3757d60930b4d98f6a9310df60ad65f1a80e2d
jghagemann <hagejotage@hotmail.com>
2021-05-11 22:02:01 -0300

    Activity Calculadora

--- app/src/main/java/br/senai/sc/senai/MainActivity.java
+++ app/src/main/java/br/senai/sc/senai/MainActivity.java
@@ -4,33 +4,159 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView display;
+    private String primeiroNumero = "";
+    private String segundoNumero = "";
+    private String operacao = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        display = findViewById(R.id.tv_display);
     }
 
-    public void onClickBtnMostrar(View v) {
-        EditText editTextTexto = findViewById(R.id.et_texto);
-        EditText editTextTexto2 = findViewById(R.id.et_texto2);
-
-        String campo1 = editTextTexto.getText().toString();
-        String campo2 = editTextTexto2.getText().toString();
-        String completo = campo1 + " " + campo2;
-
-        Toast.makeText(MainActivity.this, completo, Toast.LENGTH_LONG).show();
+    public void onClickBotao1(View v) {
+        atualizarNumeroDigitado("1");
+        atualizarDisplay("1");
     }
 
-    public void onClickBtnApagar(View v) {
-        EditText editTextTexto = findViewById(R.id.et_texto);
-        EditText editTextTexto2 = findViewById(R.id.et_texto2);
+    public void onClickBotao2(View v) {
+        atualizarNumeroDigitado("2");
+        atualizarDisplay("2");
+    }
 
-        editTextTexto.getText().clear();
-        editTextTexto2.getText().clear();
+    public void onClickBotao3(View v) {
+        atualizarNumeroDigitado("3");
+        atualizarDisplay("3");
+    }
+
+    public void onClickBotao4(View v) {
+        atualizarNumeroDigitado("4");
+        atualizarDisplay("4");
+    }
+
+    public void onClickBotao5(View v) {
+        atualizarNumeroDigitado("5");
+        atualizarDisplay("5");
+    }
+
+    public void onClickBotao6(View v) {
+        atualizarNumeroDigitado("6");
+        atualizarDisplay("6");
+    }
+
+    public void onClickBotao7(View v) {
+        atualizarNumeroDigitado("7");
+        atualizarDisplay("7");
+    }
+
+    public void onClickBotao8(View v) {
+        atualizarNumeroDigitado("8");
+        atualizarDisplay("8");
+    }
+
+    public void onClickBotao9(View v) {
+        atualizarNumeroDigitado("9");
+        atualizarDisplay("9");
+    }
+
+    public void onClickBotao0(View v) {
+        atualizarNumeroDigitado("0");
+        atualizarDisplay("0");
+    }
+
+    public void onClickAdicao(View v) {
+        if (!primeiroNumero.isEmpty()) {
+            operacao = "+";
+            atualizarDisplay("+");
+        } else {
+            Toast.makeText(MainActivity.this, "É preciso informar um número antes", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onClickSubtracao(View v) {
+        if (!primeiroNumero.isEmpty()) {
+            operacao = "-";
+            atualizarDisplay("-");
+        } else {
+            Toast.makeText(MainActivity.this, "É preciso informar um número antes", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onClickMultiplicacao(View v) {
+        if (!primeiroNumero.isEmpty()) {
+            operacao = "x";
+            atualizarDisplay("x");
+        } else {
+            Toast.makeText(MainActivity.this, "É preciso informar um número antes", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onClickDivisao(View v) {
+        if (!primeiroNumero.isEmpty()) {
+            operacao = "/";
+            atualizarDisplay("/");
+        } else {
+            Toast.makeText(MainActivity.this, "É preciso informar um número antes", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onClickApagar(View v) {
+        display.setText("");
+        primeiroNumero = "";
+        segundoNumero = "";
+        operacao = "";
+    }
+
+
+    public void onClickIgual(View v) {
+        if (!primeiroNumero.isEmpty() && !segundoNumero.isEmpty()) {
+            int numero1 = Integer.parseInt(primeiroNumero);
+            int numero2 = Integer.parseInt(segundoNumero);
+
+            if (operacao.equals("+")) {
+                int resultado = numero1 + numero2;
+                display.setText(String.valueOf(resultado));
+            }
+
+            else if (operacao.equals("-")) {
+                int resultado = numero1 - numero2;
+                display.setText(String.valueOf(resultado));
+            }
+
+            else if (operacao.equals("x")) {
+                int resultado = numero1 * numero2;
+                display.setText(String.valueOf(resultado));
+            }
+
+            else if (operacao.equals("/") && primeiroNumero.equals("0")) {
+                Toast.makeText(MainActivity.this, "Não é possível dividir por 0", Toast.LENGTH_LONG).show();
+            } else {
+                int resultado = numero1 / numero2;
+                display.setText(String.valueOf(resultado));
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "Nenhuma operação foi solicitada", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void atualizarDisplay(String texto) {
+        String textoDisplay = display.getText().toString();
+        textoDisplay = textoDisplay + texto;
+        display.setText(textoDisplay);
+    }
+
+    private void atualizarNumeroDigitado(String numero) {
+        if (operacao.isEmpty()) {
+            primeiroNumero = primeiroNumero + numero;
+        } else {
+            segundoNumero = segundoNumero + numero;
+        }
     }
 }
